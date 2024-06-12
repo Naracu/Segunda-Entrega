2f5038e0a9f2e19f2b70286f1967c1163282eedb
Daniel Lopez <g.d.lopez95@gmail.com>
2024-06-12 13:47:31 -0300

    segunda entrega

---
+++ js.js
@@ -0,0 +1,50 @@
+  function solicitarNumero(mensaje) {
+    let numero;
+    do {
+        numero = prompt(mensaje)
+    } while (isNaN(numero) || numero === null || numero.trim() === "");
+    return parseFloat(numero);
+}
+
+
+function solicitarOperacion() {
+    let operacion;
+    do {
+        operacion = prompt("Cual es la cuenta que desea realizar (+, -, *, / ):");
+    } while (operacion !== "+" && operacion !== "-" && operacion !== "*" && operacion !== "/")
+    return operacion;
+}
+
+
+let numero1 = solicitarNumero("Ingrese el primer número:")
+let numero2 = solicitarNumero("Ingrese el segundo número:");
+
+
+let operacion = solicitarOperacion();
+
+
+let resultado;
+if (operacion === "+") {
+    resultado = numero1 + numero2;
+} else if (operacion === "-") {
+    resultado = numero1 - numero2
+} else if (operacion === "/") {
+    resultado = numero1 / numero2;
+} else if (operacion === "*"){
+    resultado = numero1 * numero2;
+}
+
+
+alert("El resultado de " + numero1 + " " + operacion + " " + numero2 + " es: " + resultado);
+
+
+let titulo = document.getElementById("Titulo");
+
+titulo.innerText = "Entrega Numero 2";
+
+let subtitulo = document.getElementById("Subtitulo");
+
+subtitulo.innerText = "Esta es mi calculadora";
+
+
+Body.className = "Fondo";
