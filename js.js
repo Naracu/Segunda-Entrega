b63506b7d9d96e20101622c572cc615275cf82d3
Daniel Lopez <g.d.lopez95@gmail.com>
2024-06-29 15:00:28 -0300

    Correccion segunda entrega

--- js.js
+++ js.js
@@ -1,50 +1,50 @@
-  function solicitarNumero(mensaje) {
-    let numero;
-    do {
-        numero = prompt(mensaje)
-    } while (isNaN(numero) || numero === null || numero.trim() === "");
-    return parseFloat(numero);
-}
+//   function solicitarNumero(mensaje) {
+//     let numero;
+//     do {
+//         numero = prompt(mensaje)
+//     } while (isNaN(numero) || numero === null || numero.trim() === "");
+//     return parseFloat(numero);
+// }
 
 
-function solicitarOperacion() {
-    let operacion;
-    do {
-        operacion = prompt("Cual es la cuenta que desea realizar (+, -, *, / ):");
-    } while (operacion !== "+" && operacion !== "-" && operacion !== "*" && operacion !== "/")
-    return operacion;
-}
+// function solicitarOperacion() {
+//     let operacion;
+//     do {
+//         operacion = prompt("Cual es la cuenta que desea realizar (+, -, *, / ):");
+//     } while (operacion !== "+" && operacion !== "-" && operacion !== "*" && operacion !== "/")
+//     return operacion;
+// }
 
 
-let numero1 = solicitarNumero("Ingrese el primer número:")
-let numero2 = solicitarNumero("Ingrese el segundo número:");
+// let numero1 = solicitarNumero("Ingrese el primer número:")
+// let numero2 = solicitarNumero("Ingrese el segundo número:");
 
 
-let operacion = solicitarOperacion();
+// let operacion = solicitarOperacion();
 
 
-let resultado;
-if (operacion === "+") {
-    resultado = numero1 + numero2;
-} else if (operacion === "-") {
-    resultado = numero1 - numero2
-} else if (operacion === "/") {
-    resultado = numero1 / numero2;
-} else if (operacion === "*"){
-    resultado = numero1 * numero2;
-}
+// let resultado;
+// if (operacion === "+") {
+//     resultado = numero1 + numero2;
+// } else if (operacion === "-") {
+//     resultado = numero1 - numero2
+// } else if (operacion === "/") {
+//     resultado = numero1 / numero2;
+// } else if (operacion === "*"){
+//     resultado = numero1 * numero2;
+// }
 
 
-alert("El resultado de " + numero1 + " " + operacion + " " + numero2 + " es: " + resultado);
+// alert("El resultado de " + numero1 + " " + operacion + " " + numero2 + " es: " + resultado);
 
 
-let titulo = document.getElementById("Titulo");
+// let titulo = document.getElementById("Titulo");
 
-titulo.innerText = "Entrega Numero 2";
+// titulo.innerText = "Entrega Numero 2";
 
-let subtitulo = document.getElementById("Subtitulo");
+// let subtitulo = document.getElementById("Subtitulo");
 
-subtitulo.innerText = "Esta es mi calculadora";
+// subtitulo.innerText = "Esta es mi calculadora";
 
 
-Body.className = "Fondo";
+// Body.className = "Fondo";
